f2ecd00b360f6d938244220d37b440166a341e4c
Anthony Barba <abarba222@gmail.com>
2015-05-18 23:07:43 -0400

    background-image and javascript.

--- main.js
+++ main.js
@@ -1,7 +1,58 @@
+/*var varButton = [];
+var Equals = 0;
+
+function alertButtonValue(event){
+  var button = event.target;
+  console.log(button);
+  var text = button.textContent;
+  varButton.push(text);
+  console.log(varButton);
+
+}
+
+[].forEach.call(document.querySelectorAll('.keypad-button'), function(element){
+  element.addEventListener('click', alertButtonValue);
+}, false);
+
+
+function alertEquals(event){
+  var str = varButton.join("");
+  console.log(str);
+  Equals = eval(str);
+  console.log(Equals);
+  alert(Equals);
+}
+
+[].forEach.call(document.querySelectorAll('.equals'), function(element){
+  element.addEventListener('click', alertEquals);
+}, false);
+
+function alertClear(event){
+  Equals = 0;
+  varButton = [];
+  console.log(varButton);
+  alert(Equals);
+}
+
+[].forEach.call(document.querySelectorAll('.ac'), function(element){
+  element.addEventListener('click', alertClear);
+}, false); */
+
+
+
+
+
+
+
+
+
+
+
+
 var calculation = 0;
 var displayedCalculation = "0";
 var pendingOperation;
-var $display = document.querySelector('.display-number');
+var $display = document.querySelector('.calculations');
 
 
 function numberButtonPressed(event) {
@@ -11,6 +62,16 @@ function numberButtonPressed(event) {
         case"+":
           calculation += number;
           break;
+        case"-":
+          calculation -= number;
+          break;
+        case"*":
+          calculation *= number;
+          break;
+        case"/":
+          calculation /= number;
+          break;
+
       }
       pendingOperation = undefined;
       $display.textContent = number;
@@ -18,22 +79,21 @@ function numberButtonPressed(event) {
       displayOrConcatenateNumber(number);
     }
 
-
 }
 
 [].forEach.call(document.querySelectorAll('.keypad-button.number'), function(el){
   el.addEventListener('click',numberButtonPressed);
 }, false);
 
-/*function decimalButtonPressed(event) {
+//function decimalButtonPressed(event) {
 
-}
+//}
 
-document.querySelector('.decimal').addEventListener('click',displayOrConcatenateNumber); */
+//document.querySelector('.decimal').addEventListener('click',displayOrConcatenateNumber);
 
 function operatorButtonPressed(event) {
   pendingOperation = event.target.textContent;
-  displayOrConcatenateNumber(calculation);
+  $displayOrConcatenateNumber(calculation);
 }
 
 [].forEach.call(document.querySelectorAll('.keypad-button.operator'), function(el){
@@ -51,6 +111,8 @@ document.querySelector('.equals').addEventListener('click',equalsButtonPressed);
 
 function clearButtonPressed(event) {
   calculation = 0;
+  displayedCalculation = "0";
+  $display.textContent = calculation;
 }
 
 [].forEach.call(document.querySelectorAll('.ac'), function(el){
